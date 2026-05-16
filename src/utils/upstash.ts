838773186f624a3a985c96237b5ecac8252f6055
Vianology <oliagbenonwossi@gmail.com>
2026-05-16 12:46:11 +0000

    chore: remove unused upstash.ts file

--- src/utils/upstash.ts
+++
@@ -1,8 +0,0 @@
-import { Redis } from "@upstash/redis";
-import { env } from "../config/env";
-
-export const upstash = new Redis({
-  url: env.UPSTASH_REDIS_REST_URL,
-  token: env.UPSTASH_REDIS_REST_TOKEN,
-});
-
